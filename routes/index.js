093852c9b01572a43edc38b10fbbf698519fb969
vinothkumarr <vinothkumarr8987@gmail.com>
2024-05-02 10:00:54 +0530

    Completed

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages});
+});
+router.post('/new', function(req, res, next){
+  messages.push({text: req.body.text, user: req.body.author, added: new Date()});
+  res.redirect('/')
+})
+
+module.exports = router;
